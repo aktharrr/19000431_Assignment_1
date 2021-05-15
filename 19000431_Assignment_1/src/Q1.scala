95b3f4f251ad5a63aed1ed73f696062fe8feb64b
Akthar <aktharfarveess@gmail.com>
2021-05-15 22:44:33 +0530

    Q1 updated

---
+++ 19000431_Assignment_1/src/Q1.scala
@@ -0,0 +1,5 @@
+object Q1 extends App{
+  println("CELSIUS to FAHRENHEIT conversion\n")
+  val valueInCelsius = 35
+  println("35 degree CELSIUS in FAHRENHEIT = " + ((valueInCelsius * 1.8) + 32.0))
+}
