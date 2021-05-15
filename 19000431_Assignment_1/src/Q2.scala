c7db3793a8cd8afac5f97bbba31ef635dba97f8e
Akthar <aktharfarveess@gmail.com>
2021-05-15 22:56:52 +0530

    Q2 updated

---
+++ 19000431_Assignment_1/src/Q2.scala
@@ -0,0 +1,8 @@
+object Q2 extends App {
+  val radius = 5
+  println("Volume of a sphere with radius 5\n")
+
+  val volume = (4.0/3.0) * (math.Pi) * (math.pow(radius,3))
+
+  println("Volume = " + volume )
+}
